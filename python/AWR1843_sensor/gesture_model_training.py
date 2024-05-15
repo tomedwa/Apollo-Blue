5fe72b986ee3d35fc1ec357170cb8afc6e487c67
tom <toedwards206@gmail.com>
2024-05-16 09:46:43 +1000

    Adjusting hidden layers in training model

--- python/AWR1843_sensor/gesture_model_training.py
+++ python/AWR1843_sensor/gesture_model_training.py
@@ -1,38 +1,33 @@
 import numpy as np
 import os
-
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.callbacks import TensorBoard
-from keras.models import save_model
-
 from sklearn.model_selection import train_test_split
 
-actions = np.array(["left", "right", "up", "down"])
+# Load data
 data = np.load("combined_data.npz")
 features = data["features"]
 labels = data["labels"]
-labels_one_hot = to_categorical(labels).astype(int)
+
+features = features.reshape(features.shape[0], 1, features.shape[1])
 
 x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.2)
 
-log_dir = os.path.join("Logs")
-tb_callback = TensorBoard(log_dir=log_dir)
-
-shape = features.shape
-print(shape)
-
+# Define model
 model = Sequential()
-model.add(Dense(64, activation="relu", input_shape=(60,)))
+model.add(LSTM(64, input_shape=(x_train.shape[1], x_train.shape[2])))
 model.add(Dense(32, activation="relu"))
 model.add(Dense(4, activation="softmax"))
-
 
 # Compile the model
 model.compile(
     optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
+
+# Callbacks
+log_dir = os.path.join("Logs")
+tb_callback = TensorBoard(log_dir=log_dir)
 
 # Train the model
 history = model.fit(
@@ -50,4 +45,5 @@ test_loss, test_accuracy = model.evaluate(x_test, y_test)
 print("Test Loss:", test_loss)
 print("Test Accuracy:", test_accuracy)
 
-model.save("gesture_recognition_model.keras")
+# Save the model
+model.save("gesture_recognition_model.h5")
